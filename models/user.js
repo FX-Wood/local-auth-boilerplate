e510e69e14a252b065d06431631e184cc6f1587c
FX-Wood <fxwood2@gmail.com>
2019-02-28 11:20:06 -0800

    Added user model validation, committing before migration

--- models/user.js
+++ models/user.js
@@ -1,9 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Invalid user name. Musct be between 1 and 99 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {msg: "Invalid email address"}
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: "Password must be at least 8 characters."
+        }
+      }
+    }
   }, {});
   user.associate = function(models) {
     // associations can be defined here
