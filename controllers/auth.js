b6b17abe2d33070254d19b2d3e18e152a2e63068
FX-Wood <fxwood2@gmail.com>
2019-03-01 09:14:07 -0800

    commit before day 2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
 
+router.post('/signup', function(req,res) {
+  db.user.findOrCreate({
+    where: {email: req.body.email},
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    if (created) {
+      console.log('User created');
+      res.redirect("/");
+    } else {
+      console.log('Email already exists');
+      res.redirect("/auth/signup");
+    }
+  }).catch(function(error) {
+    res.redirect('/auth/signup')
+  })
+})
+
 router.get('/login', function(req, res) {
   res.render('auth/login');
 });
+
+router.post('/login', function(req, res) {
+  res.redirect('/auth/login')
+})
 
 module.exports = router;
